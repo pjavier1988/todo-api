c0915a15d8153416ad9c527967d3c83cb59fe31d
unknown <pjaviercornejo@gmail.com>
2023-03-08 10:58:35 -0500

    fix(configuracion-inicial): fixing lint

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,14 +2,13 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
-        res = calc.add(5,6)
-        self.assertEqual(res,11)
-
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res = calc.subtract(10,6)
-        self.assertEqual(res,4)
-
+        res = calc.subtract(10, 6)
+        self.assertEqual(res, 4)
